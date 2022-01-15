fe379fd865a30ff38c5bb7ef5af02f9102eabbc9
Ricardo Morales <angel.morales@est.ikiam.edu.ec>
2022-01-15 09:40:47 -0600

    Morales Angel ha resuelto el ejercicio 4

--- ejercicio_1104.sh
+++ ejercicio_1104.sh
@@ -1,2 +1,12 @@
-cd Documents/CSB/CSB/unix/data
+# Se necesitara obtener el nombre de la columna.
+# En este caso para el archivo de datos Buzzard columna 10
+cut -d ',' -f 8 data/Buzzard2015_data.csv | head -n 1
 
+# Ahora se necesita obtener el número de valores distintos.
+cut -d ',' -f 8 data/Buzzard2015_data.csv | tail -n +2 | sort | uniq | wc -l
+
+# Obtención del mínimo.
+cut -d ',' -f 8 data/Buzzard2015_data.csv | tail -n +2 | sort -n | head -n 1
+
+# Obtención del máximo.
+cut -d ',' -f 8 data/Buzzard2015_data.csv | tail -n +2 | sort -n | tail -n 1
